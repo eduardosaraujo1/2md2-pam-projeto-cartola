9d40f11d05333f4a49315d150ce80b35b21a565c
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2024-04-05 15:05:38 -0300

    (feat) grid lista jogadores

--- www/js/campo.js
+++ www/js/campo.js
@@ -1,11 +1,11 @@
 document.querySelectorAll(".jogador").forEach(e => {
     e.addEventListener("click", () => {
-        document.querySelector(".black-overlay").classList.add("active");
         document.querySelector(".lista-jogadores").classList.add("active");
+        document.querySelector(".black-overlay").classList.add("active");
     })
 })
 
 document.querySelector(".black-overlay").addEventListener("click", () => {
-    document.querySelector(".black-overlay").classList.remove("active");
     document.querySelector(".lista-jogadores").classList.remove("active");
+    document.querySelector(".black-overlay").classList.remove("active");
 })
